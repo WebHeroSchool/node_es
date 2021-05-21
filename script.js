f11dae086be78c50327120a6c65606f61ae2049e
Evgeniya <evgeniya_ignaten@mail.ru>
2021-05-21 23:05:36 +0300

    hw16 is done

--- script.js
+++ script.js
@@ -2,28 +2,13 @@ const http = require('http');
 const port = 3000;
 
 http.createServer((request, response)=>{
-    response.setHeader(
-        'Content-Type',
-        'text/html; chaset=utf-8'
-    );
-
-    if(request.url === "/home" || request.url === "/"){
-        response.write("<h2>Home</h2>");
-    }
-    else if(request.url == "/about"){
-        response.write("<h2>About</h2>");
-    }
-    else if(request.url == "/contact"){
-        response.write("<h2>Contacts</h2>");
-    }
-    else{
-        response.write("<h2>Not found</h2>");
-    }
-
-    response.end()
+    // response.write('Hello');
+    request.pipe(process.stdout)
+    response.end();
 }).listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
     console.log(`server is listening on ${port}`)
-})
+});
+
